e3b50748cb3ddb255a6cfa852db778b75ff9a269
mordehg <gilimordehayev@gmail.com>
2024-02-18 08:47:51 +0200

    first commit

---
+++ NanitHomeAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NanitHomeAssignment
+//
+//  Created by Gili Yaakov on 18/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
